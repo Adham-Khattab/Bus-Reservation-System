bf80b4e3cbd208d160ceee3bd9aeb7ae39236a4a
carolinepeter <carolinepeter22@gmail.com>
2026-07-30 01:06:10 +0300

    updated lost and found

--- src/app.js
+++ src/app.js
@@ -9,6 +9,9 @@ app.use(express.json());
 // Serve static files from /public
 app.use(express.static(path.join(__dirname, "public")));
 
+// Serve uploaded lost & found photos
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 // Mount API routes
 app.use("/api", require("./routes"));
 
